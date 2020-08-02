3a91195ea7ac3a5bf06c1e04a569f5aaed205a36
SharpKoi <30861942+SharpKoi@users.noreply.github.com>
2020-08-02 15:44:36 +0800

    add youtube download feature

--- pykachu.py
+++ pykachu.py
@@ -1,3 +1,4 @@
+import os
 from __future__ import unicode_literals
 
 from flask import Flask, request, abort
@@ -10,9 +11,7 @@ import random
 import configparser
 import json
 
-import clock
-from clock import *
-
+from yt_downloader import Downloader
 
 app = Flask(__name__)
 
@@ -31,11 +30,12 @@ GOOGLE_DRIVE_URL = 'https://drive.google.com/drive/folders/13YSMSTgxpUFwUzXafZbv
 WEBSITE_URL = 'https://pykachu-website.herokuapp.com/resources.jsp'
 
 help_board = '========小幫手========\n' \
-             ';;bot   - 功能介面\n' \
-             ';;md    - 查看HackMD筆記\n' \
-             ';;drive - 查看共享資料\n' \
-             ';;web   - 查看網站(比賽資料)\n' \
-             ';;bye   - 掰掰'
+             ';;bot                                       - 功能介面\n' \
+             ';;md                                        - 查看HackMD筆記\n' \
+             ';;drive                                     - 查看共享資料\n' \
+             ';;web                                       - 查看網站(比賽資料)\n' \
+             ';;dl [audio(-a)|video(-v)] [youtube url]    - 從youtube下載影片或音樂\n' \
+             ';;bye                                       - 掰掰'
 
 leave_msg_list = ['掰掰♪ 有需要再找我呦~',
                   '怎麼趕我走! QAQ',
@@ -44,8 +44,6 @@ leave_msg_list = ['掰掰♪ 有需要再找我呦~',
 
 # flex json files
 data_ui_file = open('flex/data_ui.json', mode='r', encoding='utf-8-sig', errors='ignore')
-reminder_ui_file = open('flex/reminder_datetime_ui.json', mode='r', encoding='utf-8-sig', errors='ignore')
-remind_confirm_ui_file = open('flex/remind_confirm_ui.json', mode='r', encoding='utf-8-sig', errors='ignore')
 
 # cache json
 setting_file_path = 'database/setting.json'
@@ -130,15 +128,36 @@ def handle_text_message(event):
             event.reply_token,
             FlexSendMessage(alt_text='資料庫', contents=data_ui())
         )
-    elif msg == ';;remind':
-        reminder_ui = json.load(reminder_ui_file)
-        reminder_ui_file.seek(0)
-        reminder_msg = FlexSendMessage(alt_text='Reminder', contents=reminder_ui)
-        line_bot_api.reply_message(
-            event.reply_token,
-            reminder_msg
-        )
-        pass
+    elif msg.startswith(';;dl') or msg.startswith(';;download'):
+        tokens = msg.split()
+        if len(tokens) < 3:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='Usage: ;;dl [audio(-a)|video(-v)] [youtube url]')
+            )
+        media = tokens[1]
+        yt_url = tokens[2]
+        downloader = Downloader(yt_url)
+        temp_store_path = os.path.join('static/', event.source.user_id)
+
+        if media == 'audio' or media == '-a':
+            audio_path = downloader.download_audio(output_dir=temp_store_path)
+            line_bot_api.reply_message(
+                event.reply_token,
+                FileMessage(file_name=audio_path)
+            )
+        elif media == 'video' or media == '-v':
+            video_path = downloader.download_video(resolution='highest', output_dir=temp_store_path)
+            line_bot_api.reply_message(
+                event.reply_token,
+                FileMessage(file_name=video_path)
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='Usage: ;;dl [audio(-a)|video(-v)] [youtube url]')
+            )
+
     elif msg == ';;bye':
         if isinstance(event.source, SourceGroup):
             line_bot_api.reply_message(
@@ -152,29 +171,6 @@ def handle_text_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='想趕我走就說啦不用還假惺惺的道別(哭泣)'))
-    else:
-        setting_dict = get_setting()
-        reminder_setting_dict = setting_dict['reminder']
-        if user_id in reminder_setting_dict.keys():
-            e_dict = reminder_setting_dict[user_id]
-            e_dict['item'] = msg
-            put_setting(setting_dict)
-
-            remind_confirm_ui = json.load(remind_confirm_ui_file)
-            remind_confirm_ui_file.seek(0)  # when json object was loaded, we should reset the read cursor.
-
-            remind_content = remind_confirm_ui['body']['contents'][1]['contents']
-            remind_content[0]['contents'][1]['contents'][0]['text'] = e_dict['date_time']
-            remind_content[1]['contents'][1]['contents'][0]['text'] = e_dict['item']
-
-            remind_confirm_ui_msg = FlexSendMessage(
-                alt_text='Reminder confirmation page',
-                contents=remind_confirm_ui
-            )
-            line_bot_api.reply_message(
-                event.reply_token,
-                remind_confirm_ui_msg
-            )
 
 
 @handler.add(JoinEvent)
@@ -186,55 +182,6 @@ def handle_join(event):
             FlexSendMessage(alt_text='資料庫', contents=data_ui())
         ]
     )
-
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    user_id = event.source.user_id
-    data = event.postback.data
-    setting_dict = get_setting()
-    reminder_setting_dict = setting_dict['reminder']
-
-    if data == 'reminder_datetime_postback':
-        scheduled_event = ScheduledEvent(event.postback.params['datetime'], '(None)')
-
-        reminder_setting_dict[user_id] = scheduled_event.__dict__
-        put_setting(setting_dict)
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='請輸入文字訊息以設定提醒事項')
-        )
-    elif data == 'reminder_confirm_ok':
-        if user_id in reminder_setting_dict.keys():
-            e_dict = reminder_setting_dict[user_id]
-            e = ScheduledEvent(e_dict['date_time'], e_dict['item'])
-            clock.set_notify(user=user_id, event=e)
-
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='收到!! 將在 ' + e.date_time + ' 時提醒你')
-            )
-            reminder_setting_dict.pop(user_id, None)
-            put_setting(setting_dict)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='這個提醒你已經設定過囉')
-            )
-    elif data == 'reminder_confirm_cancel':
-        if user_id in reminder_setting_dict.keys():
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='已取消這次的設定')
-            )
-            reminder_setting_dict.pop(user_id, None)
-            put_setting(setting_dict)
-        else:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text='這個提醒你已經設定過囉')
-            )
 
 
 if __name__ == "__main__":
